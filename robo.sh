e6b3bbf48c56d14b211ebe7447b64060b81de237
kambar narasimha <kambarnarasimha4@gmail.com>
2026-07-28 13:35:07 +0530

    robo.sh

--- robo.sh
+++ robo.sh
@@ -3,12 +3,14 @@
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-062addd513def4ee9"
 
-for instance $@
+for instance in $@
 do
-aws ec2 run-instances
-    --image-id ami-0220d79f3f480ecf5
-    --instance-type t3.micro
-    --security-group- sg-062addd513def4ee9
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query "Reservations[*].Instances[*].PrivateIpAddress" \
+    aws ec2 run-instances 
+    --image-id ami-0220d79f3f480ecf 
+    --instance-type t3.micro 
+    --security-group- sg-062addd513def4ee9 
+    --tag-specifications'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' 
+    --query 'Instances[0].InstanceId 
+    --output text  
     --output text
 done 
