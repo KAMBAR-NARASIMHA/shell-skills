613bb221ee925b7be9a3b934a5294487baf6ee98
kambar narasimha <kambarnarasimha4@gmail.com>
2026-07-28 14:36:56 +0530

    robo.sh

--- robo.sh
+++ robo.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-AMI_ID="ami-0220d79f3f480ecf5"
-SG_ID="sg-062addd513def4ee9"
+    AMI_ID="ami-0220d79f3f480ecf5"
+    SG_ID="sg-062addd513def4ee9"
 
-for instance in $@
-do
-    aws ec2 run-instances 
-    --image-id ami-0220d79f3f480ecf5
-    --instance-type t3.micro 
-    --security-group- sg-062addd513def4ee9
-    --tag-specifications'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' 
-    --query 'Instances[0].InstanceId 
-    --output text  
+    for instance in $@
+
+do 
+
+      INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group- sg-062addd513def4ee9
+    $SG_ID    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instance[0].InstanceId'  --output text)
+
+    # gettin ip adress 
+
+    if [ $instance != "frontend" ]; then 
+        IP=$(aws describe-instances --instance-ids $INSTANCE_ID --query 'Reservation [0].Instance[0]. PrivateIpAdress' --output text )
+   else 
+      I P=$(aws describe-instances --instance-ids $INSTANCE_ID --query 'Reservation [0].Instance[0]. Pub;icIpAdress'   --output text )
+   fi
+
+     echo "$instance: $IP"
+
 done 
