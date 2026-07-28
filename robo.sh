ba3a169d22e76a3bee1381c71a5239d2dcc48ef5
kambar narasimha <kambarnarasimha4@gmail.com>
2026-07-28 13:41:35 +0530

    robo.sh

--- robo.sh
+++ robo.sh
@@ -6,11 +6,10 @@ SG_ID="sg-062addd513def4ee9"
 for instance in $@
 do
     aws ec2 run-instances 
-    --image-id ami-0220d79f3f480ecf 
+    --image-id ami-0220d79f3f480ecf5
     --instance-type t3.micro 
-    --security-group- sg-062addd513def4ee9 
+    --security-group- sg-062addd513def4ee9
     --tag-specifications'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' 
     --query 'Instances[0].InstanceId 
     --output text  
-    --output text
 done 
