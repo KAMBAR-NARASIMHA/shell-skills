c62978a260c8e3ba388433e4d819b440788c1edb
kambar narasimha <kambarnarasimha4@gmail.com>
2026-08-22 11:20:22 +0530

    11.sh

--- 11.sh
+++ 11.sh
@@ -2,6 +2,6 @@
 
 echo "please enter your PIN number"
 
-read -p PIN_NUMBER # here reads the number which you can enter 
+read -s PIN_NUMBER # here reads the number which you can enter 
 
 echo "PIN entered is number is: $PIN_NUMBER"
