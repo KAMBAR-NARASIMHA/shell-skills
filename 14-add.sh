03e94f6d0a8218b1c60d9ece571c4aea4ed4953a
kambar narasimha <kambarnarasimha4@gmail.com>
2026-08-22 14:22:00 +0530

    14-add.sh

---
+++ 14-add.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "enter your first number: "
+
+read NUM1
+
+echo "enter your second number: "
+
+read NUM2
+
+SUM=$((NUM1 + NUM2))
+
+    echo "the addition of $NUM1 and $NUM2 is: $SUM"
+
+    
