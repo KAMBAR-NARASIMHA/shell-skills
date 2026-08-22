a0660c50b900bf196e37a1b3eec60f41da6ea41d
kambar narasimha <kambarnarasimha4@gmail.com>
2026-08-22 12:21:21 +0530

    12-env.sh

--- 12-env.sh
+++ 12-env.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Course name is: $COURSE"
+START_TIME=$(date +%s)
+
+sleep 10 &
+
+END_TIME=$(dtae +%s)
+
+TOTAL_TIME=$($END_TIME-$START_TIME)
+
+echo "Script excuted in: $TOTAL_TIME seconds"
