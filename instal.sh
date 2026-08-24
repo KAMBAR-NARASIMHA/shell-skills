1c69048b49225c5177e9cf713e2f974b61d2321a
kambar narasimha <kambarnarasimha4@gmail.com>
2026-08-24 14:41:20 +0530

    instal.sh

--- instal.sh
+++ instal.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
 
 echo "ERROR: : Please run with root previage"
-
+exit 1
 fi
 
 dnf install mysql -y
@@ -13,9 +13,9 @@ dnf install mysql -y
 if [ $? -ne 0 ]; then 
 
 echo "ERROR: :Installing MYSQL"
-
+exit 1
 else  
 
 echo "Installing MYSQL is Successful"
-
+exit 1
 fi
